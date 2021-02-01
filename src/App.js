4ad76ae9ef27c2574ed26817801d687bf2b5c8b1
AntonVoronezh <pstut@mail.ru>
2021-02-01 19:02:18 +0300

    add top bar and styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Routes from "./routes";
+import TopBar from "./components/topBar";
 
 function App() {
   return (
     <div className="App">
-        aaaa
       <Router>
+      <TopBar />
         <Routes />
       </Router>
     </div>
