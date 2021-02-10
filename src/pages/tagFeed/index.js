100dd3a1f2abcb9681497c272c15a1533a9ad5e0
AntonVoronezh <pstut@mail.ru>
2021-02-10 17:16:55 +0300

    add feed

---
+++ src/pages/tagFeed/index.js
@@ -0,0 +1,58 @@
+import { stringify } from "query-string";
+import React, { Fragment, useEffect } from "react";
+
+import { useFetch } from "../../hooks/useFetch";
+import { Feed } from "../../components/feed";
+import { Pagination } from "../../components/pagination";
+import { getPaginator, limit } from "../../utils";
+import { PopularTags } from "../../components/popularTags";
+import { Loading } from "../../components/loading";
+import { ErrorMessage } from "../../components/errorMessage";
+import { FeedTogler } from "../../components/feedTogler";
+
+export const TagFeed = ({ location, match }) => {
+  const tagName = match.params.slug;
+  console.log(tagName);
+  const { offset, currentPage } = getPaginator(location);
+  const stringParameters = stringify({ offset, limit, tag: tagName });
+  const apiUrl = `/articles?${stringParameters}`;
+  const [{ isLoading, response, error }, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch, currentPage, tagName]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium clone</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <FeedTogler tagName={tagName} />
+            {isLoading && <Loading />}
+            {error && <ErrorMessage />}
+            {!isLoading && response && (
+              <Fragment>
+                <Feed articles={response.articles}></Feed>
+                <Pagination
+                  total={response.articlesCount}
+                  url={match.url}
+                  currentPage={currentPage}
+                  limit={limit}
+                />
+              </Fragment>
+            )}
+          </div>
+          <div className="col-md-3">
+            <PopularTags />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
