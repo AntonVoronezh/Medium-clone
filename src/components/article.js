7ac7599e4a65e5bff90b49c5c8a0d26029d1c04c
AntonVoronezh <pstut@mail.ru>
2021-02-11 17:15:25 +0300

    add feed

---
+++ src/components/article.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+export const ArticleForm = ({ onSubmit, errors, initialValues }) => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [description, setDescription] = useState("");
+  const [tagList, setTagList] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ a: "aa" });
+  };
+
+  return (
+    <div className="editor-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-10 offset-md-1 col-xs-12">
+            errrors
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Article title"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                  />
+                </fieldset>
+              </fieldset>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="What is this article about?"
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
+                  />
+                </fieldset>
+              </fieldset>
+              <fieldset>
+                <fieldset className="form-group">
+                  <textarea
+                    className="form-control"
+                    rows="8"
+                    placeholder="Write your article (in markdown)"
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
+                  ></textarea>
+                </fieldset>
+              </fieldset>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Enter tags"
+                    value={tagList}
+                    onChange={(event) => setTagList(event.target.value)}
+                  />
+                </fieldset>
+              </fieldset>
+              <fieldset>
+                <fieldset className="form-group">
+                  <button
+                    type="submit"
+                    className="btn btn-lg pull-xs-right btn-primary"
+                  >
+                    Publish Article
+                  </button>
+                </fieldset>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
