d653c2162dbb8c4be5bce27737e25da52b187187
AntonVoronezh <pstut@mail.ru>
2021-02-12 17:33:25 +0300

    add tagList

--- src/components/article.js
+++ src/components/article.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {ErrorMessage} from "./errorMessage";
 
 export const ArticleForm = ({ onSubmit, errors, initialValues }) => {
   const [title, setTitle] = useState("");
@@ -8,15 +9,26 @@ export const ArticleForm = ({ onSubmit, errors, initialValues }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmit({ a: "aa" });
+    onSubmit({title, body, description, tagList});
   };
+
+  useEffect(() => {
+    if (!initialValues) {
+      return;
+    }
+
+    setTagList(initialValues.tagList.join(' '));
+    setBody(initialValues.body);
+    setTitle(initialValues.title);
+    setDescription(initialValues.description);
+  }, [initialValues]);
 
   return (
     <div className="editor-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-10 offset-md-1 col-xs-12">
-            errrors
+            {errors && <ErrorMessage errors={errors}/>}
             <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
@@ -24,8 +36,8 @@ export const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article title"
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
                   />
                 </fieldset>
               </fieldset>
@@ -35,8 +47,8 @@ export const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="What is this article about?"
-                    value={body}
-                    onChange={(event) => setBody(event.target.value)}
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
                   />
                 </fieldset>
               </fieldset>
