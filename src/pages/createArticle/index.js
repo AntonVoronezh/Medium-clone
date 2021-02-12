56f088422a4d3e03b2ef45570988ff5bc06fc010
AntonVoronezh <pstut@mail.ru>
2021-02-12 17:33:40 +0300

    add tagList

--- src/pages/createArticle/index.js
+++ src/pages/createArticle/index.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 import { ArticleForm } from "../../components/article";
+import { useFetch } from "../../hooks/useFetch";
+import { Redirect } from "react-router-dom";
+import { CurrentUserContext } from "../../contexts/currentUser";
 
 export const CreateArticle = () => {
-  const errors = {};
-  const initialValues = {};
-  const handleSubmit = (data) => {
-    console.log(data);
+  const initialValues = {
+    title: "",
+    body: "",
+    description: "",
+    tagList: [],
   };
+  const apiUrl = "/articles";
+  const [{ response, error }, doFetch] = useFetch(apiUrl);
+  const [isSuccessfullSubmit, setIsSucsessfullSubmit] = useState(false);
+  const [currentUserState] = useContext(CurrentUserContext);
+
+  const handleSubmit = (article) => {
+    doFetch({
+      method: "post",
+      data: {
+        article,
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (!response) {
+      return;
+    }
+
+    setIsSucsessfullSubmit(true);
+  }, [response]);
+
+  if (isSuccessfullSubmit) {
+    return <Redirect to={`/articles/${response.article.slug}`} />;
+  }
+
+  if (currentUserState.isLoggedIn === false) {
+    return <Redirect to={`/`} />;
+  }
+
   return (
     <div>
       <ArticleForm
-        errors={errors}
+        errors={(error && error.errors) || {}}
         initialValues={initialValues}
         onSubmit={handleSubmit}
       />
